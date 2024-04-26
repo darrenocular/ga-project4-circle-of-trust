41e2d580ea82094c3d90cb8c4d3d61156fa3f2d7
Darren Leong <darrenljj07@hotmail.com>
2024-04-26 18:24:47 +0800

    Create NavBar.jsx and additional routes for /about, /explore, /host and /profile pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React, { Suspense, useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import styles from "./App.module.css";
 import AppContext from "./context/AppContext";
 import LoadingSpinner from "./components/utils/LoadingSpinner";
+import NavBar from "./components/NavBar";
 
 const Home = React.lazy(() => import("./pages/Home"));
 const Login = React.lazy(() => import("./pages/Login"));
 const Register = React.lazy(() => import("./pages/Register"));
+const Explore = React.lazy(() => import("./pages/Explore"));
+const Host = React.lazy(() => import("./pages/Host"));
+const About = React.lazy(() => import("./pages/About"));
+const Profile = React.lazy(() => import("./pages/Profile"));
 
 const App = () => {
   const [accessToken, setAccessToken] = useState("");
@@ -32,21 +38,53 @@ const App = () => {
             <p>Expiration date: {JSON.stringify(expirationDate)}</p>
             <p>Refresh token: {localStorage.getItem("refreshToken")}</p>
           </div>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                loggedInUser ? (
-                  <Navigate replace to="/home" />
-                ) : (
-                  <Navigate replace to="/login" />
-                )
-              }
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
+          <div className={styles["layout-wrapper"]}>
+            {loggedInUser && <NavBar />}
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  loggedInUser ? (
+                    <Navigate replace to="/home" />
+                  ) : (
+                    <Navigate replace to="/login" />
+                  )
+                }
+              />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route
+                path="/home"
+                element={
+                  loggedInUser ? <Home /> : <Navigate replace to="/login" />
+                }
+              />
+              <Route
+                path="/explore"
+                element={
+                  loggedInUser ? <Explore /> : <Navigate replace to="/login" />
+                }
+              />
+              <Route
+                path="/host"
+                element={
+                  loggedInUser ? <Host /> : <Navigate replace to="/login" />
+                }
+              />
+              <Route
+                path="/about"
+                element={
+                  loggedInUser ? <About /> : <Navigate replace to="/login" />
+                }
+              />
+              <Route
+                path="/profile"
+                element={
+                  loggedInUser ? <Profile /> : <Navigate replace to="/login" />
+                }
+              />
+            </Routes>
+          </div>
         </AppContext.Provider>
       </Suspense>
     </>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useContext } from "react";
+import styles from "./styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
+import AppContext from "../context/AppContext";
+import FullLogo from "../assets/logo/logo-full-color.svg";
+import Button from "../components/utils/Button";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const appContext = useContext(AppContext);
+
+  const handleLogout = () => {};
+
+  return (
+    <nav className={styles["nav"]}>
+      <div>
+        <img
+          src={FullLogo}
+          alt="full-logo"
+          className={styles["full-logo"]}
+        ></img>
+        <div className={styles["links-container"]}>
+          <NavLink
+            to="/home"
+            className={(navData) =>
+              navData.isActive ? styles["link-active"] : styles["link"]
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/explore"
+            className={(navData) =>
+              navData.isActive ? styles["link-active"] : styles["link"]
+            }
+          >
+            Explore
+          </NavLink>
+          <NavLink
+            to="/host"
+            className={(navData) =>
+              navData.isActive ? styles["link-active"] : styles["link"]
+            }
+          >
+            Host
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={(navData) =>
+              navData.isActive ? styles["link-active"] : styles["link"]
+            }
+          >
+            About
+          </NavLink>
+        </div>
+      </div>
+      <div>
+        {appContext.loggedInUser.username}
+        <Button type="submit" className="logout-btn" onClick={handleLogout}>
+          Log out
+        </Button>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
