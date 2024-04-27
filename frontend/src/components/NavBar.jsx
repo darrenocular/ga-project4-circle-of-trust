317153fc6cac05e2a9e51269cb020f84bb2e4a6c
Darren Leong <darrenljj07@hotmail.com>
2024-04-27 21:09:23 +0800

    Create handleLogout() in NavBar.jsx with minor styling changes

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,14 +1,23 @@
 import React, { useContext } from "react";
 import styles from "./styles/NavBar.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import AppContext from "../context/AppContext";
 import FullLogo from "../assets/logo/logo-full-color.svg";
 import Button from "../components/utils/Button";
 
 const NavBar = () => {
   const appContext = useContext(AppContext);
+  const navigate = useNavigate();
 
-  const handleLogout = () => {};
+  const handleLogout = (e) => {
+    e.preventDefault();
+    appContext.setAccessToken("");
+    appContext.setLoggedInUser(null);
+    appContext.setExpirationDate(null);
+    if (localStorage.getItem("refreshToken"))
+      localStorage.removeItem("refreshToken");
+    navigate("/login");
+  };
 
   return (
     <nav className={styles["nav"]}>
@@ -53,8 +62,19 @@ const NavBar = () => {
           </NavLink>
         </div>
       </div>
-      <div>
-        {appContext.loggedInUser.username}
+      <div className={styles["nav-footer"]}>
+        <NavLink
+          to="/profile"
+          className={(navData) =>
+            navData.isActive ? styles["profile-active"] : styles["profile"]
+          }
+        >
+          <img
+            className={styles["profile-pic"]}
+            src={`https://getstream.io/random_svg/?name=${appContext.loggedInUser.username}`}
+          ></img>
+          <span>@{appContext.loggedInUser.username}</span>
+        </NavLink>
         <Button type="submit" className="logout-btn" onClick={handleLogout}>
           Log out
         </Button>
